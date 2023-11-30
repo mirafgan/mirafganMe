0ce6dd1817d8d0ee42637fc7b4673bf32e13068b
mirafgan <mirefqan1999@gmail.com>
2023-11-30 19:53:08 +0400

    font size

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-5xl">Mirafgan Eminbayli</h1>
+      <h1 className="md:text-5xl text-3xl">Mirafgan Eminbayli</h1>
     </div>
   )
 }
